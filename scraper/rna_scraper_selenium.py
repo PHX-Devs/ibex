8b112885eb23bd8d8de664ba1251d9e090355aa6
John Zechlin <john@phxdevs.com>
2020-05-14 15:01:07 -0700

    update algorithms to generate "result set" (set of sars2 substrings that don't match the human set)
    Co-authored-by: g3muse <g3muse@gmail.com>

--- scraper/rna_scraper_selenium.py
+++ scraper/rna_scraper_selenium.py
@@ -74,7 +74,7 @@ def substringFinder(string1, string2, minimum_length=2):
 
     return match_list
 
-def allSubstr(instring, candidate, min_length=0):
+def allMatchingSubstrings(instring, candidate, min_length=0):
     retset = []
     #retset = set()
     #retset.add(instring)
@@ -90,7 +90,8 @@ def allSubstr(instring, candidate, min_length=0):
                 is_in = (instring[startpos:startpos+thislen] in candidate)
                 if (is_in):
                     matches_per_length += 1
-                    retset.append([startpos,startpos+thislen, is_in])
+                    # retset.append([startpos,startpos+thislen, is_in])
+                    retset.append([startpos,thislen, is_in])
                 #store it out to a db
                 iterations_counter += 1
                 if (iterations_counter > 10000000):
@@ -128,63 +129,75 @@ def getOverlayString2(sequence, match_list):
         sequence = sequence[:match[0]+1] + "x" * length + sequence[match[0] + length+1:]
     return sequence
 
+def getOverlayString3(sequence, match_list):
+    for match in match_list:
+        length = match[1] - match[0] - 1
+        sequence = sequence[:match[0]+1] + "x" * length + sequence[match[0] + length+1:]
+    return sequence
 
+def buildReferenceArray(sequence, substring_set):
+    reference_array = [0] * len(sequence)
+    
+    for substring in substring_set:
+        start = substring[0]
+        length = substring[1]
 
+        if (length == 3):
+            reference_array[start+1] = 2
+        elif (length == 4):
+            reference_array[start+1] = 1
+        elif (length > 4):
+            for i in range(start+2, start+length-2):
+                reference_array[i] = 3
+    return reference_array
+
+def buildResultSet(sequence, reference_array):
+    string_being_built = ""
+    result_set = {}
+    for i in range(0,len(sequence)):
+        if (reference_array[i] == 0):
+            string_being_built += sequence[i]
+        elif (reference_array[i] == 1):
+            string_being_built += sequence[i]
+            if (len(string_being_built) > 2):
+                result_set[string_being_built] = 1
+            string_being_built = ""
+        elif (reference_array[i] == 2):
+            string_being_built += sequence[i]
+            if (len(string_being_built) > 2):
+                result_set[string_being_built] = 1
+            string_being_built = "" + sequence[i]
+        elif (reference_array[i] == 3):
+            if (len(string_being_built) > 2):
+                result_set[string_being_built] = 1
+            string_being_built = ""
+    if (len(string_being_built) > 2):
+        result_set[string_being_built] = 1
+    return list(result_set.keys())
 
 if __name__ == "__main__":
     pp = pprint.PrettyPrinter(indent=4)
 
     print("start")
-    #human = getRNASequence("https://www.ncbi.nlm.nih.gov/nuccore/NM_001330348.2?report=fasta")
-    human = getRNASequence("https://www.ncbi.nlm.nih.gov/nuccore/NM_001382287.1?report=fasta")
+    human = getRNASequence("https://www.ncbi.nlm.nih.gov/nuccore/NM_001330348.2?report=fasta")
+    human2 = getRNASequence("https://www.ncbi.nlm.nih.gov/nuccore/NM_001382287.1?report=fasta")
+    human3 = getRNASequence("https://www.ncbi.nlm.nih.gov/nuccore/NM_004731.5?report=fasta")
+    human4 = getRNASequence("https://www.ncbi.nlm.nih.gov/nuccore/NM_001318204.2?report=fasta")
+    human5 = getRNASequence("https://www.ncbi.nlm.nih.gov/nuccore/NM_001329452.2?report=fasta")
+    human6 = getRNASequence("https://www.ncbi.nlm.nih.gov/nuccore/NM_001040662.2?report=fasta")
     sars2 = getRNASequence("https://www.ncbi.nlm.nih.gov/nuccore/MN988668.1?report=fasta")
 
 
-    # substring_set = allSubstr(human, 0)
-    # print("all the substrings (min_length=0): %s" % len(substring_set))
+    substring_set1 = allMatchingSubstrings(sars2, human, 6)
+    substring_set2 = allMatchingSubstrings(sars2, human2, 6)
+    substring_set3 = allMatchingSubstrings(sars2, human3, 6)
+    substring_set4 = allMatchingSubstrings(sars2, human4, 6)
+    substring_set5 = allMatchingSubstrings(sars2, human5, 6)
+    substring_set6 = allMatchingSubstrings(sars2, human6, 6)
 
-    # substring_set = allSubstr(human, 1)
-    # print("all the substrings (min_length=1): %s" % len(substring_set))
+    substring_set = substring_set1 + substring_set2 + substring_set3 + substring_set4 + substring_set5 + substring_set6
 
-    # substring_set = allSubstr(human, 2)
-    # print("all the substrings (min_length=2): %s" % len(substring_set))
-
-    # substring_set = allSubstr(human, 3)
-    # print("all the substrings (min_length=3): %s" % len(substring_set))
-
-    # substring_set = allSubstr(human, 4)
-    # print("all the substrings (min_length=4): %s" % len(substring_set))
-
-    # substring_set = allSubstr(human, 5)
-    # print("all the substrings (min_length=5): %s" % len(substring_set))
-
-    # substring_set = allSubstr(human, 6)
-    # print("all the substrings (min_length=6): %s" % len(substring_set))
-
-    # substring_set = allSubstr(human, 7)
-    # print("all the substrings (min_length=7): %s" % len(substring_set))
-
-    substring_set = allSubstr(sars2, human, 5)
-    
-
-    #print(substring_set[0:100])
-
-    #substrings = substringFinder(human, sars2, 5)
-
-    
-    #pp.pprint(substrings)
-    #print("found %s substrings" % len(substrings))
-
-    #test = getOverlayString("TTAAJOHNZECHLIN", [{'end': 3, 'match': 'TTAA', 'start': 0},])
-    # expecting ABCDxxxxxJKLMNO
-    #0123456789 1011121314
-    #ABCxxxxxIJ K L M N O
-
-    test = getOverlayString2(sars2, substring_set)
-    print(test)
-
-    print("all the sars2 substrings (min_length=7): %s" % len(substring_set))
-    print("analyzed %s substrings" % len(substring_set))
-
-
+    reference_array = buildReferenceArray(sars2, substring_set)
+    result_set = buildResultSet(sars2, reference_array)
+    print (result_set)
     print("end")
